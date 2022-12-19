19be0776e9c05f4d62b51c50b7aa203795e16862
Alessandro Vaiz <alessandro.vaiz@mblabs.com.br>
2022-12-19 14:26:06 -0300

    chore: remove unused files

--- provider/index.ts
+++
@@ -1,9 +0,0 @@
-import { container } from 'tsyringe';
-
-import { IEmailProvider } from '@shared/container/providers/emailProvider/emailProvider.domain';
-import InstantMessengerEmailProvider from '@shared/container/providers/emailProvider/instantMessenger/instantMessengerEmailProvider';
-
-container.registerSingleton<IEmailProvider>(
-  'EmailProvider',
-  InstantMessengerEmailProvider,
-);
